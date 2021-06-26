8ebdb2d87187dfcbfd98a1be2cf87dd62a15590f
TouKhen <jack.lecomtes@gmail.com>
2021-06-26 19:39:28 +0200

    core error fixed

--- public/index.php
+++ public/index.php
@@ -1,2 +1,7 @@
 <?php
 	require_once '../app/require.php';
+
+	// Check if the url is "blog_ajax" to fix the error
+	if ($_SERVER['REQUEST_URI'] === "/blog_ajax/") {
+	    header("Location: " . URL_ROOT . "/index");
+    }
